4c9e99bd4f01df46f56b60f06731c7e65d8e2d62
pavelmzs <uporov155@gmail.com>
2022-07-04 12:09:05 +0300

    1

--- Proga/Proga/MainWindow.xaml.cs
+++ Proga/Proga/MainWindow.xaml.cs
@@ -23,6 +23,16 @@ namespace Proga
         public MainWindow()
         {
             InitializeComponent();
+            double[] dataX = new double[] { 5432, 3566, 3467, 6578, 5322 };
+            DateTime[] date = new DateTime[5];
+            for(int i = 0; i < date.Length; i++)
+            {
+                date[i] = new DateTime(2020, 11, 1).AddDays(1 * i);
+            }
+            double[] da = date.Select(x => x.ToOADate()).ToArray();
+            Chart.Plot.AddScatter(da, dataX);
+            Chart.Plot.XAxis.DateTimeFormat(true);
+            Chart.Refresh();
         }
     }
 }
